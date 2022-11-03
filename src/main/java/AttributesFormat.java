bb99043ab219c4ff405b5d9b95c1a58621075505
omoluabidotcom <yyahaya222@yahoo.com>
2022-11-03 12:57:33 +0100

    Attributes class format add

---
+++ src/main/java/AttributesFormat.java
@@ -0,0 +1,24 @@
+public class AttributesFormat {
+
+    String trait_type;
+    String value;
+
+    public AttributesFormat() {}
+
+    public void setTrait_type(String trait_type) {
+        this.trait_type = trait_type;
+    }
+
+    public String getTrait_type() {
+        return trait_type;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
